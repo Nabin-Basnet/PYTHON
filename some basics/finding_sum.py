d399557484ef23495ecf97e5b1be7456978a7f6a
nabin1200 <nabinbasnet1200@gmail.com>
2025-02-18 09:56:58 +0545

    add in python

---
+++ some basics/finding_sum.py
@@ -0,0 +1,12 @@
+# num1=int(input("enter the number number 1:"))
+# num2=int(input("enter the number number 2:"))
+# sum=num1+num2
+# print("sum of the number you provided is ", sum ,"hello")
+
+
+num=int(input("enter how many number you want:"))
+sum=0
+for i in range(0,num):
+    number=int(input("enter number:"))
+    sum+=number
+print("sum of the number you provided is :",sum)
