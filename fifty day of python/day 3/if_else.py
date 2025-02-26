e90096a258f62723b68c78519a9094f47c4b4e35
nabin1200 <nabinbasnet1200@gmail.com>
2025-02-26 17:23:15 +0545

    learn about if else statement

---
+++ fifty day of python/day 3/if_else.py
@@ -0,0 +1,16 @@
+# Python Conditions and If statements
+a=20
+b=30
+
+if a>b :
+    print("true")
+else:
+    print("false")
+
+print("a is greater") if a>b else print("bis greater")
+print("A") if a > b else print("=") if a == b else print("B")
+print("A") if a > b else print("=") if a == b else print("B")
+
+# if statements cannot be empty, but if you for some reason have an if statement with no content, put in the pass statement to avoid getting an error.
+if b > a:
+  pass
