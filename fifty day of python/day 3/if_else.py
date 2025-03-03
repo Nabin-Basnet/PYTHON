9ff46f9cdadd7f121e37986aa650274c91d12a04
nabin1200 <nabinbasnet1200@gmail.com>
2025-03-03 12:17:03 +0545

    function

--- fifty day of python/day 3/if_else.py
+++ fifty day of python/day 3/if_else.py
@@ -11,6 +11,8 @@ print("a is greater") if a>b else print("bis greater")
 print("A") if a > b else print("=") if a == b else print("B")
 print("A") if a > b else print("=") if a == b else print("B")
 
-# if statements cannot be empty, but if you for some reason have an if statement with no content, put in the pass statement to avoid getting an error.
+# if statements cannot be empty, but if you for 
+# some reason have an if statement with no content, 
+# put in the pass statement to avoid getting an error.
 if b > a:
   pass
--- fifty day of python/day 5/function.py
+++ fifty day of python/day 5/function.py
@@ -1,23 +1,70 @@
-def my_functionn():
-  print("Hello from a function")
+# In Python a function is defined using the def keyword:
+a=20
+b=30
+# creating the function
+def my_function():
+    print("hello world")
 
-my_functionn()
-
-
-def my_function(fname):
-  print(fname + " Refsnes")
-
-my_function("Emil")
-my_function("Tobias")
-my_function("Linus")
+# calling the function
+my_function()
 
 
-def myy_function(fname, lname):
-  print(fname + " " + lname)
+#creating function with arguments
+def function_argu(fname):
+    print("my name is "+fname)
 
-myy_function("Emil")
+#passing an argument
+function_argu("nabin")
+#
 
-def my_function(child3, child2, child1):
-  print("The youngest child is " + child3)
+# function to calculate sum with parameters  
+def sum(x,y):
+    add=x+y
+    print(add)
+    return add
 
-my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+# calling the function which name is sum
+result=sum(a,b)
+print(result)
+
+# By default, a function must be called with the correct number of 
+# arguments. Meaning that if your function expects 2 arguments, you have 
+# to call the function with 2 arguments, not more, and not less.
+
+# If you do not know how many arguments that will be passed into your 
+# function, add a * before the parameter name in the function definition.
+
+def my_kids(*kids):
+    print("i have many child among them eldest is "+kids[0])
+
+my_kids("nabin","ram","shyam")
+
+# You can also send arguments with the key = value syntax.
+def my_childs(child1,child2,child3):
+    print("my childs are "+child1+","+child2+","+child3)
+
+my_childs(child1="hem",child3="ayush",child2="mohit")
+
+
+# If you do not know how many keyword arguments that will be passed into 
+# your function, add two asterisk: ** before the parameter name in the
+#  function definition.
+
+def key_arguments(**kid):
+    print("his last name is "+kid["lname"])
+
+key_arguments(fname="nabin",lname="basnet")
+
+# defaunt parameters
+def default_parameters(country="nepal"):
+    print("i am from "+country)
+
+default_parameters("USA")
+default_parameters()
+
+
+# returm value
+def return_val(x):
+    return 5*x
+
+print(return_val(5))
