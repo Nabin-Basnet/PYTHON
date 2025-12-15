3179f49c16b589379469803236b9d66e15bec39b
nabin <nabinbasnet1200@gmail.com>
2025-12-15 08:31:43 +0545

    update

--- some basics/Data type/List.py
+++ some basics/Data type/List.py
@@ -4,3 +4,21 @@ fruits=['apple','banana', 'orange' ,'papaya']
 print(fruits)
 
 # List items are ordered, changeable, and allow duplicate values.
+lists=["apple",24,32,"mango"]  #creation of list
+lists[2]=49 #replace the item at the index
+lists.append(34)  #add an item at last of the list
+lists.insert(2,"orange") #add item at any index
+lists.pop(1)  #remove an item at the given index
+lists.remove("mango")  #remove the items by it name
+print(lists)
+
+
+
+#looping the list item
+for x in lists:
+    print(x)
+
+
+
+lists.clear()   #clear the all item in the list
+print(lists)
---
+++ some basics/Data type/tupel.py
@@ -0,0 +1,4 @@
+#tupel is also a data type which is used to store the multiple values
+
+tupel=("apple","banana","cat","dog","eagle")
+print(tupel)
