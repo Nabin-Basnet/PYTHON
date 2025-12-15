bb982cdb16206204f87bb29d624320c037eac2a3
nabin <nabinbasnet1200@gmail.com>
2025-12-15 14:45:17 +0545

    dictionaries

--- some basics/Data type/dist.py
+++ some basics/Data type/dist.py
@@ -1,5 +1,30 @@
-mydict={
-    "name":"nabin"
+mydict={                         #creating the the dictionaries
+    "name":"nabin",
+    "address":"belbari",
+    "age":20,
+    "school":"itahari namuna"
 }
 
-print(mydict)
+print(mydict)     #access all data
+print(mydict["address"])   #accessing the value using key 
+
+
+
+mydict["name"]="sahabir"  #change the value
+mydict.update({"age":21})   #update the dictionaries
+
+mydict["father"]="myfather"  #add an item in dictionaries
+
+mydict.pop("father")  #remove the items
+
+for d in mydict:
+    print(mydict[d])   #this print the value
+
+
+
+for d in mydict:
+    print(d)         #print the key 
+
+
+for x, y in mydict.items():  #loop throw key value
+    print(f"{x}: {y}")
