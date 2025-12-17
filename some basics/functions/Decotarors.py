d0eacd73d091fc82419795f2602d08c4be702d6e
nabin <nabinbasnet1200@gmail.com>
2025-12-17 19:32:43 +0545

    decorator

--- some basics/functions/Decotarors.py
+++ some basics/functions/Decotarors.py
@@ -1,1 +1,17 @@
 # Decorators let you add extra behavior to a function, without changing the function's code.
+#A decorator is a function that takes another function as input and returns a new function.
+
+
+def initalgreet(func):
+    func()
+    def wrapper():
+        print("hello! babe")
+    
+    return wrapper
+
+
+@initalgreet
+def finalgreet():
+    print("how are you")
+
+finalgreet()
