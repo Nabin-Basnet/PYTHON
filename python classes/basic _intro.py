98bcf2a48700d71b04f7f3ed7e2efb2a64a33b3a
nabin <nabinbasnet1200@gmail.com>
2025-12-20 16:19:39 +0545

    basic concept of class and object

---
+++ python classes/basic _intro.py
@@ -0,0 +1,10 @@
+# Classes and objects are the two core concepts in object-oriented programming.
+# A class defines what an object should look like, and an object is created based on that class
+
+class myclass:   #to reate the class key word class is used and then the name of class
+    x=2
+
+c1=myclass()  #here c1 is the object of the class myclass
+print(c1.x)   #here x is printed from the class myclass by an object c1
+
+del c1       #here key word del delet the object c1
